42c126ac9877d93214aca6fa978697c5eb839fe9
goodude123 <assasinmcpl@gmail.com>
2019-02-23 16:36:42 +0100

    Views.

--- class_files_site/views.py
+++ class_files_site/views.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
+from .models import User, db
+import psycopg2
+
+
+conn = psycopg2.connect("dbname='lessons' user='postgres' host='localhost' password='password'")
 
 
 views = Blueprint('views', __name__, template_folder='templates')
 
 @views.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('home.html')
+    user = User('asd', 'zaq1@WSX')
+    db.session.add(user)
+    db.session.commit()
+    return render_template('foo.html')
