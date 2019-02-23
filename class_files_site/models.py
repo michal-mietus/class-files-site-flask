ae013c6d1624d740cde44c0a69dea30e3dc6ee4a
goodude123 <assasinmcpl@gmail.com>
2019-02-23 12:43:23 +0100

    Models file:   - image model for uploaded images,   - section for assign images to them

---
+++ class_files_site/models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+class BaseModel(db.Model):
+    """Base data model for all objects"""
+    __abstract__ = True
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def __repr__(self):
+        """Define a base way to print models"""
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        """
+                Define a base way to jsonify models, dealing with datetime objects
+        """
+        return {
+            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
+
+class Image(BaseModel):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key = True)
+    section_id = db.Column(db.ForeignKey('sections.id'), nullable=False)
+    name = db.Column(db.String)
+    extension = db.Column(db.String)
+    date = db.Column(db.DateTime)
+
+
+class Section(BaseModel):
+    __tablename__ = 'sections'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
